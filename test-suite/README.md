1ef787fa337a67b6d42484ac08078aaf11388a41
Martin Dørum <martid0311@gmail.com>
2025-07-27 22:20:11 +0200

    make the test suite able to run against any compatible parser program

--- test-suite/README.md
+++ test-suite/README.md
@@ -18,3 +18,19 @@ The folder `mason-suite` contains MASON-specific files. This folder contains:
 * Pairs of files starting with `y_`, one ending in `.mason` and one ending in `.json`.
   A MASON parser should parse the `.mason` file into a value that's identical
   to how a JSON parser parses the `.json` file.
+
+## Using the test runner
+
+The `run-test.js` script works as a test runner.
+Run it with: `./run-test.js <program>`,
+where `<program>` is a command-line program with your parser implementation.
+
+The program is expected to accept a path to a MASON file,
+and output a JSON string to stdout.
+The requirements are:
+
+* If parsing is successful, the program must exit with exit code 0.
+* If parsing is successful, the program must output a JSON string to stdout.
+* The JSON string that's written to stdout must represent byte strings as
+  base64-encoded JSON strings.
+* If parsing fails, the program must exit with a non-zero exit code, or a signal.
--- test-suite/run-tests.js
+++ test-suite/run-tests.js
@@ -1,12 +1,115 @@
 #!/usr/bin/env node
 
-import * as mason from "../js/mason.js"
+import { spawn } from "child_process";
 import fs from "fs";
 
+if (process.argv.length < 3) {
+	console.log("Usage: run-tests.js <parser command...>");
+	process.exit(1);
+}
+const argv = process.argv.slice(2);
+const cmd = argv[0];
+const params = argv.slice(1);
+params.push("<file>");
 let tests = 0;
 let successes = 0;
-function runJsonTests(dir) {
-	for (const name of fs.readdirSync(dir)) {
+
+function runMasonParser(path) {
+	return new Promise((resolve, reject) => {
+		params[params.length - 1] = path;
+		const child = spawn(cmd, params);
+
+		const stdouts = [];
+		const stderrs = [];
+		child.stdout.on("data", d => stdouts.push(d));
+		child.stderr.on("data", d => stderrs.push(d));
+		child.on("exit", (code, status) => {
+			const stdout = Buffer.concat(stdouts).toString("utf-8");
+			const stderr = Buffer.concat(stderrs).toString("utf-8");
+			if (status != null) {
+				reject(new Error(`Exited with status ${status}, stderr:\n${stderr}`));
+				return;
+			}
+
+			if (code != 0) {
+				reject(new Error(`Exited with code ${code}, stderr:\n${stderr}`));
+				return;
+			}
+
+			try {
+				resolve(JSON.parse(stdout));
+			} catch (err) {
+				reject(new Error(`Invalid JSON; stderr:\n${stderr}`));
+			}
+		});
+	});
+}
+
+function deepEqual(a, b) {
+	const ta = typeof a;
+	const tb = typeof b;
+	if (ta != tb) {
+		return false;
+	}
+
+	if (ta == "string" || ta == "boolean" || ta == "number" || a == null || b == null) {
+		return a == b;
+	}
+
+	if (a instanceof Array) {
+		if (!b instanceof Array) {
+			return false;
+		}
+
+		if (a.length != b.length) {
+			return false;
+		}
+
+		for (let i = 0; i < a.length; ++i) {
+			if (!deepEqual(a[i], b[i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	const keys = new Set();
+	for (const i in a) {
+		if (!a.hasOwnProperty(i)) {
+			continue;
+		}
+
+		if (!b.hasOwnProperty(i)) {
+			return false;
+		}
+
+		if (!deepEqual(a[i], b[i])) {
+			return false;
+		}
+
+		keys.add(i);
+	}
+
+	for (const i in b) {
+		if (!b.hasOwnProperty(i)) {
+			continue;
+		}
+
+		if (!keys.has(i)) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+async function runJsonTests(dir) {
+	const promises = [];
+	const promiseMap = new Map();
+	let count = 0;
+
+	async function run(name) {
 		let expectSuccess;
 		if (name.startsWith("y_")) {
 			expectSuccess = true;
@@ -14,18 +117,18 @@ function runJsonTests(dir) {
 			expectSuccess = false;
 		} else {
 			console.log(`${dir}/${name}: Unknown file name prefix`);
-			continue;
 		}
 
-		const content = fs.readFileSync(`${dir}/${name}`, "utf-8");
+		tests += 1;
+		process.stderr.write(`\r[${tests}] `);
+
 		let parseError = null;
 		try {
-			mason.parse(content);
+			await runMasonParser(`${dir}/${name}`);
 		} catch (err) {
 			parseError = err;
 		}
 
-		tests += 1;
 		if (expectSuccess && parseError) {
 			console.log(`${dir}/${name}: Expected success, but failed`);
 			console.log(parseError.stack);
@@ -34,71 +137,70 @@ function runJsonTests(dir) {
 		} else {
 			successes += 1;
 		}
+
+		return name;
 	}
-}
 
-function transformBinaryStrings(obj) {
-	if (obj instanceof Uint8Array) {
-		return Buffer.from(obj).toString("base64");
-	} else if (typeof obj == "object") {
-		for (const i in obj) {
-			if (!obj.hasOwnProperty(i)) {
-				continue;
-			}
-
-			obj[i] = transformBinaryStrings(obj[i]);
+	for (const name of fs.readdirSync(dir)) {
+		if (count >= 10) {
+			const res = await Promise.any(promises);
+			promises.splice(promises.indexOf(promiseMap.get(res)), 1);
+			promiseMap.delete(res);
+			count -= 1;
 		}
-		return obj;
-	} else {
-		return obj;
+
+		count += 1;
+		const promise = run(name);
+		promises.push(promise);
+		promiseMap.set(name, promise);
 	}
 }
 
-function runMasonTransformTest(dir, masonName) {
+async function runMasonTransformTest(dir, masonName) {
 	let jsonName = masonName.replace(/\.mason$/, ".json");
-
-	const masonContent = fs.readFileSync(`${dir}/${masonName}`, "utf-8");
 	const jsonContent = fs.readFileSync(`${dir}/${jsonName}`, "utf-8").trim();
+	const jsonObj = JSON.parse(jsonContent);
 
-	tests += 1;
-	let transformedContent;
+	let masonObj;
 	try {
-		const obj = transformBinaryStrings(mason.parse(masonContent))
-		transformedContent = JSON.stringify(obj);
+		masonObj = await runMasonParser(`${dir}/${masonName}`);
 	} catch (err) {
 		console.log(`${dir}/${masonName}: Expected success, but failed`);
-		console.log(err.stack);
+		console.log(err);
 		return;
 	}
 
-	if (transformedContent != jsonContent.trim()) {
+	if (!deepEqual(masonObj, jsonObj)) {
 		console.log(`${dir}/${masonName}: JSON transform failure`);
 		console.log("Expected:", jsonContent);
-		console.log("Got:     ", transformedContent);
+		console.log("Got:     ", JSON.stringify(masonObj));
 		return;
 	}
 
 	successes += 1;
 }
 
-function runMasonTests(dir) {
+async function runMasonTests(dir) {
 	for (const name of fs.readdirSync(dir)) {
 		if (name.endsWith(".json")) {
 			continue;
 		}
 
-		if (name.startsWith("y_")) {
-			runMasonTransformTest(dir, name);
-			continue;
-		} else if (!name.startsWith("n_")) {
+		if (!name.startsWith("n_") && !name.startsWith("y_")) {
 			console.log(`${dir}/${name}: Unknown file name prefix`);
 			continue;
 		}
 
-		const content = fs.readFileSync(`${dir}/${name}`, "utf-8");
 		tests += 1;
+		process.stderr.write(`\r[${tests}] `);
+
+		if (name.startsWith("y_")) {
+			await runMasonTransformTest(dir, name);
+			continue;
+		}
+
 		try {
-			mason.parse(content);
+			await runMasonParser(`${dir}/${name}`);
 			console.log(`${dir}/${name}: Expected failure, but succeeded`);
 		} catch {
 			successes += 1;
@@ -106,10 +208,14 @@ function runMasonTests(dir) {
 	}
 }
 
-runJsonTests("json-suite");
-runJsonTests("alt-json-suite");
-runMasonTests("mason-suite");
-console.log(`${successes}/${tests} tests succeeded.`);
-if (successes != tests) {
-	process.exit(1);
+async function runTests() {
+	await runJsonTests("json-suite");
+	await runJsonTests("alt-json-suite");
+	await runMasonTests("mason-suite");
+
+	console.log(`${successes}/${tests} tests succeeded.`);
+	if (successes != tests) {
+		process.exit(1);
+	}
 }
+runTests();
